9e304d567dd13133259a3ff8197edc16f540c9b8
Archit Kumar <75969141+Architkumar29082001@users.noreply.github.com>
2022-03-23 14:32:55 +0530

    Update DAY3.cpp

--- DAY3.cpp
+++ DAY3.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main(){
     int x;
     cin>>x;
-    if(x%2==0){
+    if(x%2==0 && x>20 ){
         cout<<"Not Weird";
         
+    }
+    else if (x%2==0 && x<=5) {
+        cout<<"Not Weird";
     }
     else {
         cout<<"Weird";
     }
 }
-
-
