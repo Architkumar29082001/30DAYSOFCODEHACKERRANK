ab16d7d6d5bd9da629ce7bb5d250c5fc74c484ef
Archit Kumar <75969141+Architkumar29082001@users.noreply.github.com>
2022-03-26 12:15:38 +0530

    Create DAY7.cpp

---
+++ DAY7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    int arr[num];
+    for (int i = 0; i < num; i++) {
+        cin >> arr[i];
+    }
+    for (int j = num - 1; j >= 0; j--) {
+        cout << arr[j] << " ";
+    }
+    return 0;
+}
